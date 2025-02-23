ab99cc434e640263cb15c8dcf60b0a8e552bbbf3
Darren-KL <qdw0310@gmail.com>
2025-02-23 20:26:41 +0800

    丘丘人和岩盔王

---
+++ Assets/Models/丘丘岩盔王/Stonehide.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using System.Collections;
+
+public class StoneHideController : MonoBehaviour
+{
+    private Animator animator;
+    private bool isTakingDamage = false;
+    public GameObject deathEffectPrefab; // 砞竚疭
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetBool("getHit", false);
+    }
+
+    void Update()
+    {
+        // フ龄秨﹍计
+        if (Input.GetKeyDown(KeyCode.Space) && !isTakingDamage)
+        {
+            StartCoroutine(TakeDamageSequence());
+        }
+    }
+
+    IEnumerator TakeDamageSequence()
+    {
+        isTakingDamage = true;
+        // ち传阑笆礶
+        animator.SetBool("Hit", true);
+
+        // 单5
+        yield return new WaitForSeconds(3f);
+
+        // ち传笆礶
+        animator.SetBool("Hit", false);
+        animator.SetTrigger("Die");
+
+        // 单0.5綪反
+        yield return new WaitForSeconds(1f);
+        Destroy(gameObject);
+
+        //ネΘ疭
+        if (deathEffectPrefab != null)
+        {
+            Instantiate(deathEffectPrefab, transform.position, Quaternion.identity);
+        }
+    }
+}
