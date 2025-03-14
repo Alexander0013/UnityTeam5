c6f237fb49b3ec10e1e48bc18f7ded23ae9af30e
Alex Liang <alexliang@Alexdebijixingdiannao.local>
2025-03-14 13:02:28 +0800

    final boss FSM

--- Assets/丘丘岩盔王0305/Scripts/BossBaseState.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public abstract class BossBaseState
-{
-    public abstract void EnterState(BossFSM boss);
-    public abstract void UpdateState(BossFSM boss);
-    public abstract void ExitState(BossFSM boss);
-}
