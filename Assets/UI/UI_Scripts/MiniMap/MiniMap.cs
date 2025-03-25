4128a4cf7c6d24533656828a3458bb0156d1c012
m6153246 <m6153246@gmail.com>
2025-03-25 21:11:19 +0800

    minimap

--- Assets/UI/UI_Scripts/MiniMap/MiniMap.cs
+++ Assets/UI/UI_Scripts/MiniMap/MiniMap.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MiniMap : MonoBehaviour
 {
     Transform playerTransform;
-    Transform mainCamera;
+    Camera mainCamera;
     GameObject Player_A;
     GameObject Player_B;
 
@@ -27,7 +27,12 @@ public class MiniMap : MonoBehaviour
         Player_A = characterManager.characters[0];
         Player_B = characterManager.characters[1];
         playerTransform = Player_A.transform;
-        mainCamera = Camera.main.transform;
+        mainCamera = Camera.main;
+
+        if (characterManager == null) { Debug.Log("characterManager == null"); }
+        if (Player_A == null) { Debug.Log("Player_A == null"); }
+        if (Player_B == null) { Debug.Log("Player_B == null"); }
+        if (mainCamera == null) { Debug.Log("mainCamera == null"); }
     }
 
     private void switchFollower()
@@ -49,6 +54,6 @@ public class MiniMap : MonoBehaviour
         Vector3 newPosition = playerTransform.position;
         newPosition.y = base.transform.position.y;
         base.transform.position = newPosition;
-        base.transform.rotation = Quaternion.Euler(90f,mainCamera.eulerAngles.y, 0);
+        base.transform.rotation = Quaternion.Euler(90f,mainCamera.transform.eulerAngles.y, 0);
     }
 }
